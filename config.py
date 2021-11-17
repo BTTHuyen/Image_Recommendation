b74a75510fe4e328fd8c6db4d312cbbcf8476550
BTTHuyen <buithanhhuyen.567@gmail.com>
2021-11-17 16:13:52 +0900

    config appId and App password

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "b69a4680-b985-425c-8446-24b0b778efdc")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "rccms2021")
